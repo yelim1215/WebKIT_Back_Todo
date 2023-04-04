3e9d2b557e3560886f3e3ad554e83a215300b907
yelim1215 <yerim1215jg@naver.com>
2023-04-04 19:41:22 +0900

    23. 프로젝트 실습 #3 CRUD

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,5 +73,63 @@ public class TodoController {
 		
 		// HTTP Status 200 상태로 response를 전송한다.
 		return ResponseEntity.ok().body(response);
+	}
+	
+	@GetMapping("/update")
+	public ResponseEntity<?>update(@RequestBody TodoDTO dto) {
+		try {
+			// dto 를 이용해 테이블에 저장하기 위한 entity를 생성한다.
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			
+			// entity userId를 임시로 지정한다.
+			entity.setUserId("temporary-user");
+			
+			// service.create 를 통해 repository 에 entity를 저장한다.
+			// 이때 넘어노는 값이 없을 수도 있으므로 List가 아닌 Optional로 한다.
+			Optional<TodoEntity> entities = service.update(entity);
+			
+			// entites 를 dtos로 스트림 변환한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			
+			// Response DTO 를 생성한다.
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			
+			// HTTP Status 200 상태로 response를 전송한다.
+			return ResponseEntity.ok().body(response);
+		}
+		catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
+	}
+	
+	@PutMapping
+	public ResponseEntity<?>updateTodo(@RequestBody TodoDTO dto) {
+		try {
+			// dto 를 이용해 테이블에 저장하기 위한 entity를 생성한다.
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			
+			// entity userId를 임시로 지정한다.
+			entity.setUserId("temporary-user");
+			
+			// service.create 를 통해 repository에 entity를 저장한다.
+			// 이때 넘어오는 값이 없을 수도 있으므로 List가 아닌 Optional로 한다.
+			Optional<TodoEntity> entities = service.updateTodo(entity);
+			
+			// entities 를 dto 로 스트림 변환한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			
+			// Response DTO를 생성한다.
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			
+			// HTTP Status 200 상태로 response를 전송한다.
+			return ResponseEntity.ok().body(response);
+		}
+		catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
 	}
 }
--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -2,13 +2,11 @@ package com.example.todo.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.todo.model.TodoEntity;
 import com.example.todo.persistence.TodoRepository;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -28,6 +26,41 @@ public class TodoService {
 	public List<TodoEntity>retrieve(final String userId) {
 		return repository.findByUserId(userId);
 	}
+	
+	public Optional<TodoEntity>update(final TodoEntity entity) {
+		// Validations
+		validate(entity);
+		if (repository.existsById(entity.getId())) {
+			repository.save(entity);
+		}
+		else
+			throw new RuntimeException("Unknown id");
+		
+		return repository.findById(entity.getId());
+	}
+	
+	public Optional<TodoEntity>updateTodo(final TodoEntity entity) {
+		// Validations
+		validate(entity);
+		
+		// 테이블에서 id에 해당하는 데이터셋을 가져온다.
+		final Optional<TodoEntity> original = repository.findById(entity.getId());
+		
+		// orignal 에 담겨진 내용을 todo 에 할당하고 title, done 값을 변경한다.
+		original.ifPresent(todo -> {
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.isDone());
+			repository.save(todo);
+		});
+		/* 위의 람다식과 동일한 표현
+		*if (original.isPresent()) {
+		*	final TodoEntity todo = original.get();
+		*	todo.setTitle(entity.getTitle());
+		*	todo.setDone(entity.isDone());
+		*	repository.save(todo);
+		*/
+		return repository.findById(entity.getId());
+		}
 	
 	public void validate(final TodoEntity entity) {
 		if (entity == null) {
